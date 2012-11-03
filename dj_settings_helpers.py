2448b2c43269f98dec9ece8721eaba87a1ccec6c
Travis Swicegood <development@domain51.com>
2012-11-02 21:44:51 -0500

    Add get_env function

--- dj_settings_helpers.py
+++ dj_settings_helpers.py
@@ -23,3 +23,53 @@ def create_project_dir(project_path):
         return os.path.join(base, *paths)
 
     return project_dir
+
+
+def get_env(key, default, force_bool=False, type_func=None, **defaults):
+    """
+    Returns the correct environment variable
+
+    This provides a few useful additions on top of a generic call to
+    ``os.environ.get(key, default)``.  First, you can coerce a value
+    into a boolean by using the kwarg ``force_bool=True``.  For example:
+
+    ::
+
+        >>> import os
+        >>> os.environ['some-value'] = 'Yes'
+        >>> get_env('some-value', 'default', force_bool=True)
+        True
+
+    This works with ``yes``, ``true``, and ``1``.  Capitalization is
+    ignored for all of these.
+
+    Next, you can provide a ``type_func`` to force a conversion to a
+    particular type.  This is useful for values that need to be an
+    integer, since everything in ``os.environ`` is a string.  For
+    example:
+
+    ::
+
+        >>> os.environ['some-int'] = '1234'
+        >>> get_env('some-int', 'default', type_func=int)
+        1234
+
+    Finally, you can provide extra defaults that change depending on the
+    environment.  For example, let's say you have a dev and testing
+    environment you can use ``get_env`` like this:
+
+    ::
+
+        >>> os.environ['ENVIRONMENT'] = 'testing'
+        >>> get_env('some-key', 'default', dev='dev', testing='testing')
+        'testing'
+        >>> os.environ['ENVIRONMENT'] = 'dev'
+        >>> get_env('some-key', 'default', dev='dev', testing='testing')
+        'dev'
+
+    """
+    env = os.environ.get('ENVIRONMENT')
+    a = os.environ.get(key, defaults.get(env, default))
+    if force_bool:
+        return str(a).lower() in ['1', 'yes', 'true', ]
+    return a if type_func is None else type_func(a)
--- tests.py
+++ tests.py
@@ -1,8 +1,9 @@
 import os
+import random
 import types
 import unittest
 
-from dj_settings_helpers import create_project_dir
+from dj_settings_helpers import create_project_dir, get_env
 
 
 class TestOfProjectDirGenerator(unittest.TestCase):
@@ -22,3 +23,56 @@ class TestOfProjectDirGenerator(unittest.TestCase):
         project_dir = create_project_dir('/foo/settings.py')
         expected = self.generate_expected_path('foo', 'templates', 'config')
         self.assertEqual(expected, project_dir('templates', 'config'))
+
+
+class TestOfGenEnv(unittest.TestCase):
+    def test_returns_default_if_no_env_var_set(self):
+        default = 'foobar'
+        self.assertEqual(default, get_env('unknown-and-unknowable', default))
+
+    def test_returns_env_var_if_set(self):
+        a = '%s' % random.randint(1000, 2000)
+        os.environ['some-test-key-%s' % a] = a
+        self.assertEqual(a, get_env('some-test-key-%s' % a, 'default'))
+        del os.environ['some-test-key-%s' % a]
+
+    def test_returns_different_default_based_on_environment(self):
+        default = 'foobar'
+        dev_default = 'barfoo'
+        os.environ['ENVIRONMENT'] = 'dev'
+
+        self.assertEqual(dev_default, get_env('not-set', default,
+                dev=dev_default))
+
+    def test_can_force_to_boolean(self):
+        self.assertTrue(get_env('foo', True, force_bool=True) is True)
+        self.assertTrue(get_env('foo', '1', force_bool=True) is True)
+        self.assertTrue(get_env('foo', 'yes', force_bool=True) is True)
+        self.assertTrue(get_env('foo', 'true', force_bool=True) is True)
+        self.assertTrue(get_env('foo', 'True', force_bool=True) is True)
+        self.assertTrue(get_env('foo', 'TRUE', force_bool=True) is True)
+
+        self.assertTrue(get_env('foo', False, force_bool=True) is False)
+        self.assertTrue(get_env('foo', '0', force_bool=True) is False)
+        self.assertTrue(get_env('foo', 'no', force_bool=True) is False)
+        self.assertTrue(get_env('foo', 'false', force_bool=True) is False)
+        self.assertTrue(get_env('foo', 'False', force_bool=True) is False)
+        self.assertTrue(get_env('foo', 'FALSE', force_bool=True) is False)
+
+
+    def test_can_be_coerced_by_a_func(self):
+        def type_func(value):
+            self.assertEqual(value, 'Hi, Dad!')
+            return 'Hi, Mom!'
+
+        result = get_env('foo', 'Hi, Dad!', type_func=type_func)
+        self.assertEqual('Hi, Mom!', result)
+
+    def test_coercion_function_receives_returned_value(self):
+        value = 'foo-%s' % random.randint(1000, 2000)
+        os.environ['random-value'] = value
+
+        def type_func(a):
+            self.assertEqual(a, value)
+
+        get_env('random-value', 'default', type_func=type_func)
